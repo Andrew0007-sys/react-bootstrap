7d304ac85bc9488a0748c7a7408f4685d117bcea
Andrew0007-sys <55852364+Andrew0007-sys@users.noreply.github.com>
2020-05-30 19:24:32 +0300

    test2

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -7,30 +7,28 @@ function Footer() {
     return(
         <div className='Footer'>
             <div className='container justify-content-between'>    
-                <div className='row'>
-                    
-                    <div className='col-lg-3 col-md-6 col-sm-10'>
-                        <img className='item-right' src={logo1} alt='itCluster-logo' /> 
-                    </div>
-                    
-                    <div className='item-right col-lg-3 col-md-6 col-sm-10'>
+                <div className='row otstup justify-content-between'>
+                        <img className='item-left col-lg-2 col-md-3 col-sm-2' src={logo1} alt='itCluster-logo' /> 
+                    <div className='item-left col-lg-3 col-md-6 col-sm-10'>
                         ICON1 ICON2 ICON3 
                     </div>
                 </div>
-                <div className='row'>
-                    
-                    <div className='item-right col-lg-3 col-md-6 col-sm-10'>
+                <div className='row  otstup justify-content-between'>            
+                    <div className='item-left size-ft col-lg-4 col-md-6 col-sm-10'>
                         IT Cluster Academy<br />
-                        Телефон: <span>+38 (063) 674 12 19</span><br/>
-                        Пошта: <span>recruitacademyit@gmail.com</span><br/>
+                        Телефон: <span className='violet-text'>+38 (063) 674 12 19</span><br/>
+                        Пошта: <span className='violet-text'>recruitacademyit@gmail.com</span><br/>
                     </div>
-                    <div className='item-right col-lg-3 col-md-6 col-sm-10'>
-                        <p>IT Cluster Academy online</p>
-                        <p>Телефон: <span>+38 (098) 655 45 29</span></p>
-                        <p>Пошта: <span>itcaedu.online@gmail.com</span></p>
+                    <div className='item-left size-ft col-lg-4 col-md-6 col-sm-10'>
+                        IT Cluster Academy online<br />
+                        Телефон: <span className='violet-text'>+38 (098) 655 45 29</span><br />
+                        Пошта: <span className='violet-text'>itcaedu.online@gmail.com</span><br />
 
                     </div>
                 </div>
+            </div>
+            <div className='container-fluid violet-back'>
+                <p>© 2020 | Copyright | All rights reserved</p>
             </div>
         </div>
     )
